19ab887ed364e7c5635ea5b2d51c64feeefde34e
tanyinghui <yhtan927@gmail.com>
2019-02-18 14:47:14 +0800

    add new function to apply subnet mask

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ IP address and port scanner written in nodeJS
 
 ## Requirement 
 - NodeJS version 10.13.0
+
+## Run the code
+- Run `npm install` to install the dependencies.
 - Use `node portchecker.js` to run the code.
--- portchecker.js
+++ portchecker.js
@@ -1,44 +1,37 @@
 var readline = require('readline');
 var net = require('net');
+var ip = require('ip');
+var Netmask = require('netmask').Netmask;
+
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 var timeout = 5000;
 
-rl.question('IP address: ', (ip) => {
-    rl.question('Start Port: ', (port) => {
-        rl.question('Port Array (ranging from 1 to 65535): ', (portArray) => {
-            console.log(`Your IP address is ${ip}. Start Port is ${port}. Port Array is ${portArray}.`);
-            // function parseIP(ip) {
-            //     if(ip.match(/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3})/)!=null)  {
-            //         ip = ip.match(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/);  //clean posible port or http://
-            //         return ip.split(".");   //returns [a,b,c,d] array
-            //     }
-            //     else {
-            //         return false;
-            //     }
-            // };
-            // console.log(ip);
+ipAdd = ip.address();
+console.log('Your IP address: ' + ipAdd);
+
+let block = new Netmask(ipAdd + '/24');
+rl.question('Start Port: ', (port) => {
+    rl.question('Port Array: ', (portArray) => {
+        block.forEach( (ip, long, index) => {
             var Ports = Array.from(Array(parseInt(portArray)), (x,i) => i+parseInt(port));
+            
             for (portx in Ports) {
                 (function(portx) {
                     var s = new net.Socket();
                     s.setTimeout(timeout, function() { s.destroy(); });
-                    s.connect(portx, ip, function() {
-                        console.log('OPEN: ' + portx);
-                        console.log(s.address())
-                    });
-                    s.on('data', function(data) {
-                        console.log(portx +': '+ data);
-                        s.destroy();
+                    s.connect(Ports[portx], ip, function() {
+                        console.log('OPEN: ' + ip + ':' + Ports[portx]);
                     });
                     s.on('error', function(e) {
                         s.destroy();
                     });
                 })(portx);
-            }; 
-            rl.close();
-        })
+            };
+        
+        });
+        rl.close();
     })
 });
