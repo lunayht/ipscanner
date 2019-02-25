9076c53698382692f2b484121b7024f763924ab2
tanyinghui <yhtan927@gmail.com>
2019-02-25 12:40:45 +0800

    add ping func to complete scanning process

--- portchecker.js
+++ portchecker.js
@@ -1,9 +1,9 @@
 const readline = require('readline');
-const net = require('net');
 const ip = require('ip');
 const Netmask = require('netmask').Netmask;
 const http = require('http');
-const keepAliveAgent = new http.Agent({  maxSockets:100});
+const ping = require('ping');
+const keepAliveAgent = new http.Agent({ maxSockets:100});
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -13,43 +13,38 @@ var timeout = 5000;
 ipAdd = ip.address();
 console.log('Your IP address: ' + ipAdd);
 
-let block = new Netmask(ipAdd + '/24');
+let block = new Netmask(ipAdd + '/16');
 rl.question('Start Port: ', (port) => {
     rl.question('Port Array: ', (portArray) => {
-        // http.Agent.Socket.
-
-        var Ports = Array.from(Array(parseInt(portArray)), (x,i) => i+parseInt(port));
-        // console.log(Ports);
-        console.time("start");
-
+        var Ports = Array.from(Array(parseInt(portArray)), (x,i) => i+parseInt(port));var IPs = [];
         block.forEach( (ip, long, index) => {
-            for (portx in Ports) {
-                (function(portx) {
-                    //var s = new net.Socket();
-                    var s = keepAliveAgent.createConnection({host: ip,port: Ports[portx], family:4},() =>{
-                        console.log('REPLY: ' + ip + ':' + Ports[portx])
-                        console.log()
-                        
-                    });
-                    s.on('data', function(e){
-                        console.log('DATA: ' + ip + ':' + Ports[portx] + ", response: " + e.toString())
-
-
-                    })
-                    s.setTimeout(timeout, function() { 
-                       //  console.log('NO REPLY: ' + ip + ':' + Ports[portx])
-
-                        s.end(); });
-
-                    s.on('error', function(e) {
-                        s.end();
-                    });
-                    
-
-                })(portx);
-            };
+            IPs.push(ip);
         });
-        console.timeEnd("start");
+        IPs.forEach(function(host){
+            ping.sys.probe(host, function(isAlive){
+                if (isAlive) {
+                    for (portx in Ports) {
+                        (function(portx) {
+                            var s = keepAliveAgent.createConnection({
+                                host: host,
+                                port: Ports[portx], 
+                                family:4}, () =>{
+                                console.log('OPEN: ' + host + ':' + Ports[portx])
+                            });
+                            s.on('data', function(data){
+                                console.log('DATA: ' + host + ':' + Ports[portx] + ", response: " + data)
+                            });
+                            s.setTimeout(timeout, function() { 
+                                s.destroy(); 
+                            });
+                            s.on('error', function() {
+                                s.destroy();
+                            });
+                        })(portx);
+                    };
+                }
+            });
+        });
         rl.close();
     })
 });
--- testing.js
+++ testing.js
@@ -3,9 +3,12 @@ const ip = require('ip');
 const net = require('net');
 const Netmask = require('netmask').Netmask;
 const http = require('http');
+const ping = require('ping');
 const ipAdd = ip.address();
 const timeout = 5000;
-const keepAliveAgent = new http.Agent();
+const keepAliveAgent = new http.Agent({
+    maxSockets: 100,
+});
 console.log('Your IP address: ' + ipAdd);
 let block = new Netmask(ipAdd + '/24');
 
@@ -15,44 +18,38 @@ askQuestions([
 ]).then(answers => {
     var port = parseInt(answers[0]);
     var portArray = parseInt(answers[1]);
-    const n = portArray/200;
-    for (var k = 0; k < n; k++) {
-        var Ports = Array.from(Array(200), (x,i) => i+parseInt(port));
-        block.forEach( (ip, long, index) => {
-            for (portx in Ports) {
-                (function(portx) {
-                    var s = keepAliveAgent.createConnection({
-                        host: ip,
-                        port: Ports[portx], 
-                        family:4}, () =>{
-                        console.log('OPEN: ' + ip + ':' + Ports[portx])
-                    });
-                    s.on('data', function(data){
-                        console.log('DATA: ' + ip + ':' + Ports[portx] + ", response: " + data)
-                    })
-                    s.setTimeout(timeout, function() { 
-                        s.destroy(); });
-
-                    s.on('error', function() {
-                        s.destroy();
-                    });
-                    // var s = new net.Socket();
-                    // s.setTimeout(timeout, function() { s.destroy(); });
-                    // s.connect(Ports[portx], ip, function() {
-                    //     console.log('OPEN: ' + ip + ':' + Ports[portx]);
-                    // });
-                    // s.on('data', function(data) {
-                    //     console.log(ip + ':' + port + ': '+ data);
-                    //     s.destroy();
-                    // });
-                    // s.on('error', function(e) {
-                    //     s.destroy();
-                    // });
-                })(portx);
-            };
+    var Ports = Array.from(Array(portArray), (x,i) => i+port);
+    var IPs = [];
+    block.forEach( (ip, long, index) => {
+        IPs.push(ip);
+    });
+    IPs.forEach(function(host){
+        ping.sys.probe(host, function(isAlive){
+            if (isAlive) {
+                console.log(host);
+                for (portx in Ports) {
+                    (function(portx) {
+                        var s = keepAliveAgent.createConnection({
+                            host: host,
+                            port: Ports[portx], 
+                            family:4}, () =>{
+                            console.log('OPEN: ' + host + ':' + Ports[portx])
+                        });
+                        s.on('data', function(data){
+                            console.log('DATA: ' + host + ':' + Ports[portx] + ", response: " + data)
+                        });
+                        s.setTimeout(timeout, function() { 
+                            s.destroy(); 
+                        });
+                        s.on('error', function() {
+                            s.destroy();
+                        });
+                    })(portx);
+                };
+            }
         });
-        //console.log(n);
-        //console.log(Ports);
-        port += 200;
-    }
+    });
 });
+
+
+
